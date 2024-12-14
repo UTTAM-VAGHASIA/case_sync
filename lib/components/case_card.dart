3ce5442c634f72da5e47237bd5120f655a067283
Uttam <161334079+internatwork@users.noreply.github.com>
2024-12-14 14:58:33 +0530

    Update case_card.dart

--- lib/components/case_card.dart
+++ lib/components/case_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/case_list.dart';
+import '../screens/cases/caseinfo.dart';
 
 class CaseCard extends StatelessWidget {
   final CaseListData caseItem;
@@ -14,53 +15,65 @@ class CaseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-      color: isHighlighted ? Colors.black : Colors.white,
-      elevation: 3,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-          side: BorderSide(color: Colors.black, style: BorderStyle.solid)),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Case No: ${caseItem.caseNo}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: isHighlighted ? Colors.white : Colors.black,
-              ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CaseInfoPage(
+              caseId: caseItem.caseNo, // Pass case_id here
             ),
-            const SizedBox(height: 5),
-            Text(
-              '${caseItem.applicant} vs ${caseItem.opponent}',
-              style: TextStyle(
-                fontSize: 14,
-                color: isHighlighted ? Colors.white : Colors.black,
+          ),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+        color: isHighlighted ? Colors.black : Colors.white,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: Colors.black, style: BorderStyle.solid)),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Case No: ${caseItem.caseNo}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: isHighlighted ? Colors.white : Colors.black,
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'Summon Date: ${caseItem.srDate.day.toString().padLeft(2, '0')}/'
-              '${caseItem.srDate.month.toString().padLeft(2, '0')}/'
-              '${caseItem.srDate.year}',
-              style: TextStyle(
-                fontSize: 14,
-                color: isHighlighted ? Colors.white : Colors.black,
+              const SizedBox(height: 5),
+              Text(
+                '${caseItem.applicant} vs ${caseItem.opponent}',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: isHighlighted ? Colors.white : Colors.black,
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'Court Name: ${caseItem.courtName}, ${caseItem.cityName}',
-              style: TextStyle(
-                fontSize: 14,
-                color: isHighlighted ? Colors.white : Colors.black,
+              const SizedBox(height: 5),
+              Text(
+                'Summon Date: ${caseItem.srDate.day.toString().padLeft(2, '0')}/'
+                '${caseItem.srDate.month.toString().padLeft(2, '0')}/'
+                '${caseItem.srDate.year}',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: isHighlighted ? Colors.white : Colors.black,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 5),
+              Text(
+                'Court Name: ${caseItem.courtName}, ${caseItem.cityName}',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: isHighlighted ? Colors.white : Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
